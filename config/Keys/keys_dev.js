d9555c70f21a692a9892e3606cd49367aae6bdf0
UmairMalikk <umair.malik@bykea.com>
2019-01-04 14:57:34 +0500

    mongoose connectivity done

---
+++ config/Keys/keys_dev.js
@@ -0,0 +1,4 @@
+module.exports = {
+  mongoURI: "mongodb://angular:angular1@ds149404.mlab.com:49404/angular-node",
+  secretOrKey: "secret"
+};
--- server.js
+++ server.js
@@ -1,21 +1,50 @@
-const express = require('express')
-const morgan  = require('morgan')
-const session = require('express-session')
-const cookieParser = require('cookie-parser')
+const express = require("express");
+const morgan = require("morgan");
+const session = require("express-session");
+const cookieParser = require("cookie-parser");
+const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
 
-const app = express()
-const port  = process.env.port || 8000;
+const dbPath = require("./config/config").mongoURI;
 
-app.use(morgan('dev'))
+const app = express();
+const port = process.env.port || 8000;
 
-app.use('/',(req,res)=>{
-    res.send('hello')
-})
- 
-app.listen(port,(err)=>{
-    if(err){
-        console.log(err)
-    }else{
-        console.log(`Server is connected on ${port}`)
+mongoose.connect(
+  dbPath,
+  err => {
+    if (err) {
+      console.log(err);
+    } else {
+      console.log("Database is Connected");
     }
-})
+  }
+);
+
+app.use(morgan("dev"));
+app.use(cookieParser());
+app.use(
+  session({
+    secret: "angular-node-connectivity",
+    saveUninitialized: true,
+    resave: true
+  })
+);
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use("view engine", "ejs");
+
+app.use("/", (req, res) => {
+  res.send("hello");
+  console.log(req.cookies);
+  console.log("=======================");
+  console.log(req.session);
+});
+
+app.listen(port, err => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log(`Server is connected on ${port}`);
+  }
+});
